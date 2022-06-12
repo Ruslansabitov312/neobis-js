91719c1899259dff60a243b68ee9662dc5604175
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-06-12 17:43:27 +0600

    Fix bugs and add draw button

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-window.onload = function () {
+// window.onload = function () {
+//   const canvas = document.getElementById('canvas')
+//   const ctx = canvas.getContext('2d')
+
+//   ctx.beginPath()
+//   //arc(x,y,r,sAngle,eAngle,counterclockwise);
+//   ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // Outer circle
+//   ctx.moveTo(110, 75)
+//   ctx.arc(75, 75, 35, 0, Math.PI, false) // Mouth (clockwise)
+//   ctx.moveTo(65, 65)
+//   ctx.arc(60, 65, 5, 0, Math.PI * 2, true) // Left eye
+//   ctx.moveTo(95, 65)
+//   ctx.arc(90, 65, 5, 0, Math.PI * 2, true) // Right eye
+//   ctx.stroke()
+// }
+
+function drawSmile() {
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
 
